54b01921ed4d9ae5d3a5c19a7646b1a846882e1b
Alagunambi Welkin <alagunambiwelkin@gmail.com>
2012-05-17 16:22:11 +0530

    Changed user login from email to username

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120427070841) do
+ActiveRecord::Schema.define(:version => 20120517102234) do
 
   create_table "bills", :force => true do |t|
     t.string   "code"
@@ -93,6 +93,7 @@ ActiveRecord::Schema.define(:version => 20120427070841) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
+    t.string   "username"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
